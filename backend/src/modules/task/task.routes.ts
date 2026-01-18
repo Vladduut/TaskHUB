efc379740d52514a63b1c79fd3a608eea6644ffc
Vlaadut <farimavladut@yahoo.com>
2026-01-18 12:32:48 +0200

    Fix prisma ESM imports

--- backend/src/modules/task/task.routes.ts
+++ backend/src/modules/task/task.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../lib/prisma.js";
 
 export async function taskRoutes(app: FastifyInstance) {
   // list tasks for a project
